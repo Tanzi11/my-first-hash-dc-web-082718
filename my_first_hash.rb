617088f417bc7608395ada4a21f7c3e01a255c4b
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-06 18:36:03 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash = {"color" => "red",}
+def my_hash = {"color" => "red", "design" => "stripes"}
   
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
